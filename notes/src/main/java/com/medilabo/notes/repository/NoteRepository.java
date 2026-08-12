1084832cd00553aea46efcced0c1ac4529a11842
Mika.el <lebris.mika@gmail.com>
2026-08-12 19:30:49 +0200

    sprint 2 , start Note with MongoDb

--- notes/src/main/java/com/medilabo/notes/repository/NoteRepository.java
+++ notes/src/main/java/com/medilabo/notes/repository/NoteRepository.java
@@ -2,9 +2,11 @@ package com.medilabo.notes.repository;
 
 import com.medilabo.notes.model.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
     List<Note> findByPatId(Long patId);
 }
